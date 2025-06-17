ae56e5903eb8d4f48b54e6763532754a32e5742b
Gavtriple9 <29155514+Gavtriple9@users.noreply.github.com>
2025-06-17 00:17:33 -0700

    Add project layout

---
+++ src/sensors/attitude_estimator.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file attitude_estimator.cpp
+ * @author Gavin Roberts (admin@gavinsroberts.com)
+ * @brief
+ * @version 0.1
+ * @date 2025-06-17
+ *
+ * @copyright Copyright (c) 2025
+ *
+ * Combines IMU data (gyro + accel + mag)
+ * Uses EKF library for sensor fusion
+ * Outputs quaternion-based attitude
+ */
